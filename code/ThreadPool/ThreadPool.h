633224a507ee2338a31507608e86547933e1d29a
wumr <784716883@qq.com>
2021-03-26 21:06:48 +0800

    创建项目 Signed-off-by: wumr <784716883@qq.com>

---
+++ code/ThreadPool/ThreadPool.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <thread>
+
+
+class ThreadPool
+{
+public:
+
+
+private:
+	using Task = std::function<void()>;
+	//线程池
+
+	//任务队列
+
+	//同步
+
+	//条件阻塞
+
+	//是否关闭提交
+
+	//空闲线程数量
+
+};
---
+++ code/ThreadPool/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
